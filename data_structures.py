19c2e4fb00e99d5d9cbe6e77eebe15c37b3df3e3
Vini-Araujoo <viniciustrabalhosunb@gmail.com>
2026-06-21 20:55:20 -0300

    feat: implementar LinkedList e metodo append em tempo O(1)

--- data_structures.py
+++ data_structures.py
@@ -4,3 +4,21 @@ class Node:
         self.key = key
         self.value = value
         self.next = None
+
+class LinkedList:
+    """A singly linked list with a tail pointer for O(1) appends."""
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def append(self, key, value=None):
+        """Append a new node to the end of the list in O(1) time."""
+        new_node = Node(key, value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
