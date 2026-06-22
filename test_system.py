bc9eb43fa0f4a7d7f83790bf5a04daf00e154730
Ricardo-909-Leal <ricardoeguiff@email.com>
2026-06-21 22:15:32 -0300

    test: cria estrutura inicial de testes

---
+++ test_system.py
@@ -0,0 +1,4 @@
+import unittest
+from data_structures import Node, LinkedList, HashTable, Vertex, Graph
+from preprocessing import preprocess_tweet
+from opinion_mining import build_base_graph, classify_tweet
